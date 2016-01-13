fc431e4370d6944cb922eb1adba1ec0f11a6373e
Chax-Fortress <nikolasp@ifi.uio.no>
2016-01-13 01:28:19 +0100

    Added more stuff for coverage

--- BF4-Bot/Python/MouseMovement.py
+++ BF4-Bot/Python/MouseMovement.py
@@ -1,9 +1,16 @@
-def click():
-    print "Implement click"
+import win32gui
+import win32api
 
+class MouseMovement:
 
-def offsetClick():
-    print "Implement offsetClick"
+    def click():
+        print "Implement click"
 
-def toCoord():
-    print "Implement toCoord"
+    def offsetClick():
+        print "Implement offsetClick"
+
+    def toCoord():
+        print "Implement toCoord"
+
+    def __init__(self,title = "Battlefield 4"):
+        self.title = title
--- BF4-Bot/Python/PixelSearch.py
+++ BF4-Bot/Python/PixelSearch.py
@@ -1,13 +1,16 @@
 import win32gui
 import win32api
 
+class PixelSearch:
 
+    def pixel_search():
+        print "Implement pixel_search"
 
-def get_red_pos(maxTry = 10):
-    print "Implement get_red_pos"
+    def grab_screen():
+        print "Implement grab_screen"
 
-def pixel_search():
-    print "Implement pixel_search"
+    def __init__(self, title = "Battlefield 4"):
+        self.title = title
 
-def grab_screen():
-    print "Implement grab_screen"
+    def get_red_pos(maxTry = 10):
+        print "Implement get_red_pos"
