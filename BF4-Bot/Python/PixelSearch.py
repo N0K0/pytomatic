83cf02d3acebc1050e341412b1c1ba067dad1826
Chax-Fortress <nikolasp@ifi.uio.no>
2016-02-12 00:41:12 +0100

    Cleanup

--- BF4-Bot/Python/PixelSearch.py
+++ BF4-Bot/Python/PixelSearch.py
@@ -18,7 +18,10 @@ class PixelSearch:
         temp_img = ImageGrab.grab(self.wh.create_boundingbox(self.wh.hwnd))
         temp_img.save('test.jpg','JPEG')
         self.last_image = temp_img
-        print "Implement grab_screen"
+        return temp_img
+
+    def img_to_numpy(self,image):
+
 
 
     def get_red_pos(self,maxTry = 10):
@@ -26,6 +29,3 @@ class PixelSearch:
         print "Implement get_red_pos"
 
         return bbox
-
-if __name__ == '__main__':
-    hwnd = _get_hwnd_by_title("Calculator")
