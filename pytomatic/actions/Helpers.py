3daef98956116e167a4e8ba86052cde9fcb2b0de
Nikolas <nikolasp@ifi.uio.no>
2017-03-16 23:14:10 +0100

    Added waitbar

--- pytomatic/actions/Helpers.py
+++ pytomatic/actions/Helpers.py
@@ -1,10 +1,17 @@
 from matplotlib import pyplot
-
-class Helpers:
-    @staticmethod
-    def show_matrix(matrix):
-        pyplot.imshow(matrix,cmap='hot')
-        pyplot.show()
+from time import sleep
+import sys
 
 
+def show_matrix(matrix):
+    pyplot.imshow(matrix,cmap='hot')
+    pyplot.show()
 
+def waiting_bar(time, steps = 20):
+    print("Waiting {}".format(time))
+    for step in range(steps+1):
+        sys.stdout.write("[{}{}]".format("#"*step,'.'*(steps - step)))
+        sys.stdout.flush()
+        sleep(time/steps)
+        sys.stdout.write('\r')
+    print("")
